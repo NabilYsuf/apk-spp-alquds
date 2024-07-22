4f3037e441bf425b607319b02ef9d8898b5f0f5f
Ysufff <onlyoneniqtophileee@gmail.com>
2024-07-22 15:46:10 +0800

    "Removed Bulan Ajaran field and added Tahun Ajaran and Bulan Tagihan fields in pembayaran_spp.php and tagihan.php; updated queries and form fields in proses_tagihan.php"

--- admin/hapususer.php
+++ admin/hapususer.php
@@ -1,20 +1,33 @@
 <?php
+// Memulai sesi
+session_start();
+
 // Konfigurasi koneksi database
 include 'koneksi.php';
 
-// Mendapatkan ID kelas yang akan dihapus
-$id_user = $_GET['id']; // Anda dapat menggunakan metode lain untuk mendapatkan ID ini
+// Mendapatkan ID pengguna yang sedang login dari sesi
+$id_user_logged_in = $_SESSION['id_user'];
 
-// Membuat query SQL untuk menghapus data
-$sql = "DELETE FROM tb_user WHERE id_user = $id_user";
+// Mendapatkan ID pengguna yang akan dihapus
+$id_user_to_delete = $_GET['id']; // Anda dapat menggunakan metode lain untuk mendapatkan ID ini
 
-// Menjalankan query untuk menghapus data
-if ($koneksi->query($sql) === TRUE) {
+// Periksa apakah pengguna yang sedang login mencoba menghapus dirinya sendiri
+if ($id_user_to_delete == $id_user_logged_in) {
     echo '<script language="javascript" type="text/javascript">
-      alert("Data user berhasil dihapus.");</script>';
+      alert("Anda tidak dapat menghapus pengguna yang sedang login.");</script>';
     echo "<meta http-equiv='refresh' content='0; url=user.php'>";
 } else {
-    echo "Error: " . $sql . "<br>" . $koneksi->error;
+    // Membuat query SQL untuk menghapus data
+    $sql = "DELETE FROM tb_user WHERE id_user = $id_user_to_delete";
+
+    // Menjalankan query untuk menghapus data
+    if ($koneksi->query($sql) === TRUE) {
+        echo '<script language="javascript" type="text/javascript">
+          alert("Data user berhasil dihapus.");</script>';
+        echo "<meta http-equiv='refresh' content='0; url=user.php'>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $koneksi->error;
+    }
 }
 
 // Menutup koneksi database
